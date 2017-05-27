83ab11f6ec4beda6f6308083775d04956e1012a4
Joss Brown <joss.brown@gmx.net>
2017-05-27 18:52:24 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# notify
+![notify-platform-macos](https://img.shields.io/badge/platform-macOS-lightgrey.svg)
+![notify-code-shell](https://img.shields.io/badge/code-shell-yellow.svg)
+[![notify-depend-tnote](https://img.shields.io/badge/dependency-terminal--notifier-green.svg)](https://github.com/alloy/terminal-notifier)
+[![notify-license](http://img.shields.io/badge/license-MIT+-blue.svg)](https://github.com/JayBrown/notify/blob/master/LICENSE)
+
+# notify <img src="https://github.com/JayBrown/notify/blob/master/img/jb-img.png" height="20px"/>
+**Template shell script function & code for macOS notifications using terminal-notifier**
+
+`notify "subtitle" "message"`
---
+++ notify.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+ACCOUNT=$(/usr/bin/id -un)
+PROCESS="Process Name" # exchange for actual process name depending on your script
+ICON_LOC="/.VolumeIcon.icns" # exchange for actual path to the icon image file
+
+# notify function
+notify () {
+ 	if [[ "$TN_STATUS" == "osa" ]] ; then # notify with AppleScript
+		/usr/bin/osascript &>/dev/null << EOT
+tell application "System Events"
+	display notification "$2" with title "$PROCESS [" & "$ACCOUNT" & "]" subtitle "$1"
+end tell
+EOT
+	elif [[ "$TN_STATUS" == "tn-app" ]] ; then # old method with terminal-notifier.app
+		"$TN_LOC/Contents/MacOS/terminal-notifier" \
+			-title "$PROCESS [$ACCOUNT]" \
+			-subtitle "$1" \
+			-message "$2" \
+			-appIcon "$ICON_LOC" \
+			>/dev/null
+	elif [[ "$TN_STATUS" == "tn-cli" ]] ; then # new method with terminal-notifier
+		"$TN" \
+			-title "$PROCESS [$ACCOUNT]" \
+			-subtitle "$1" \
+			-message "$2" \
+			-sender Finder \
+			-appIcon "$ICON_LOC" \
+			>/dev/null
+	fi
+}
+
+# look for terminal-notifier
+TN=$(which terminal-notifier)
+if [[ "$TN" == "" ]] || [[ "$TN" == *"not found" ]] ; then
+	TN_LOC=$(/usr/bin/mdfind "kMDItemCFBundleIdentifier == 'nl.superalloy.oss.terminal-notifier'" 2>/dev/null | /usr/bin/awk 'NR==1')
+	if [[ "$TN_LOC" == "" ]] ; then
+		TN_STATUS="osa"
+	else
+		TN_STATUS="tn-app"
+	fi
+else
+	TN_VERS=$("$TN" -help | /usr/bin/head -1 | /usr/bin/awk -F'[()]' '{print $2}' | /usr/bin/awk -F. '{print $1"."$2}')
+	if [[ "$TN_VERS" == "" ]] || (( $(echo "$TN_VERS < 1.8" | /usr/bin/bc -l) )) ; then
+		TN_LOC=$(/usr/bin/mdfind "kMDItemCFBundleIdentifier == 'nl.superalloy.oss.terminal-notifier'" 2>/dev/null | /usr/bin/awk 'NR==1')
+		if [[ "$TN_LOC" == "" ]] ; then
+			TN_STATUS="osa"
+		else
+			TN_STATUS="tn-app"
+		fi
+	else
+		TN_STATUS="tn-cli"
+	fi
+fi
+
+# notify: exchange subtitle & message text for the actual strings
+notify "subtitle" "message text"
