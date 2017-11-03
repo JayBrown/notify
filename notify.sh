553183fe83d5dab36e72cdb0a3b442296d3ec00f
Joss Brown <joss.brown@gmx.net>
2017-11-03 15:36:54 +0100

    update for tn v2.0.0

--- notify.sh
+++ notify.sh
@@ -1,56 +1,77 @@
 #!/bin/bash
 
-ACCOUNT=$(/usr/bin/id -un)
-PROCESS="Process Name" # exchange for actual process name depending on your script
-ICON_LOC="/.VolumeIcon.icns" # exchange for actual path to the icon image file
+# updated for terminal-notifier v2.0.0: https://github.com/julienXX/terminal-notifier
+
+account=$(/usr/bin/id -un)
+process="Process Name" # exchange for actual process name depending on your script
+icon_loc="/.VolumeIcon.icns" # exchange for actual path to the icon image file
 
 # notify function
 notify () {
- 	if [[ "$TN_STATUS" == "osa" ]] ; then # notify with AppleScript
-		/usr/bin/osascript &>/dev/null << EOT
+ 	if [[ "$tn_status" == "osa" ]] ; then
+		osascript &>/dev/null << EOT
 tell application "System Events"
-	display notification "$2" with title "$PROCESS [" & "$ACCOUNT" & "]" subtitle "$1"
+	display notification "$2" with title "$process [" & "$account" & "]" subtitle "$1"
 end tell
 EOT
-	elif [[ "$TN_STATUS" == "tn-app" ]] ; then # old method with terminal-notifier.app
-		"$TN_LOC/Contents/MacOS/terminal-notifier" \
-			-title "$PROCESS [$ACCOUNT]" \
+	elif [[ "$tn_status" == "tn-app-new" ]] || [[ "$tn_status" == "tn-app-old" ]] ; then
+		"$tn_loc/Contents/MacOS/terminal-notifier" \
+			-title "$process [$account]" \
 			-subtitle "$1" \
 			-message "$2" \
-			-appIcon "$ICON_LOC" \
+			-appIcon "$icon_loc" \
 			>/dev/null
-	elif [[ "$TN_STATUS" == "tn-cli" ]] ; then # new method with terminal-notifier
-		"$TN" \
-			-title "$PROCESS [$ACCOUNT]" \
+	elif [[ "$tn_status" == "tn-cli" ]] ; then
+		"$tn" \
+			-title "$process [$account]" \
 			-subtitle "$1" \
 			-message "$2" \
-			-appIcon "$ICON_LOC" \
+			-appIcon "$icon_loc" \
 			>/dev/null
 	fi
 }
 
 # look for terminal-notifier
-TN=$(which terminal-notifier)
-if [[ "$TN" == "" ]] || [[ "$TN" == *"not found" ]] ; then
-	TN_LOC=$(/usr/bin/mdfind "kMDItemCFBundleIdentifier == 'nl.superalloy.oss.terminal-notifier'" 2>/dev/null | /usr/bin/awk 'NR==1')
-	if [[ "$TN_LOC" == "" ]] ; then
-		TN_STATUS="osa"
-	else
-		TN_STATUS="tn-app"
-	fi
-else
-	TN_VERS=$("$TN" -help | /usr/bin/head -1 | /usr/bin/awk -F'[()]' '{print $2}' | /usr/bin/awk -F. '{print $1"."$2}')
-	if [[ "$TN_VERS" == "" ]] || (( $(echo "$TN_VERS < 1.8" | /usr/bin/bc -l) )) ; then
-		TN_LOC=$(/usr/bin/mdfind "kMDItemCFBundleIdentifier == 'nl.superalloy.oss.terminal-notifier'" 2>/dev/null | /usr/bin/awk 'NR==1')
-		if [[ "$TN_LOC" == "" ]] ; then
-			TN_STATUS="osa"
+tn=$(which terminal-notifier 2>/dev/null)
+if [[ "$tn" == "" ]] || [[ "$tn" == *"not found" ]] ; then
+	tn_loc=$(mdfind "kMDItemCFBundleIdentifier == 'fr.julienxx.oss.terminal-notifier'" 2>/dev/null | awk 'NR==1')
+	if [[ "$tn_loc" == "" ]] ; then
+		tn_loc=$(mdfind "kMDItemCFBundleIdentifier == 'nl.superalloy.oss.terminal-notifier'" 2>/dev/null | awk 'NR==1')
+		if [[ "$tn_loc" == "" ]] ; then
+			tn_status="osa"
 		else
-			TN_STATUS="tn-app"
+			tn_status="tn-app-old"
 		fi
 	else
-		TN_STATUS="tn-cli"
+		tn_status="tn-app-new"
+	fi
+else
+	tn_vers=$("$tn" -help | head -1 | awk -F'[()]' '{print $2}' | awk -F. '{print $1"."$2}')
+	if (( $(echo "$tn_vers >= 1.8" | bc -l) )) && (( $(echo "$tn_vers < 2.0" | bc -l) )) ; then
+		tn_status="tn-cli"
+	else
+		tn_loc=$(mdfind "kMDItemCFBundleIdentifier == 'fr.julienxx.oss.terminal-notifier'" 2>/dev/null | awk 'NR==1')
+		if [[ "$tn_loc" == "" ]] ; then
+			tn_loc=$(mdfind "kMDItemCFBundleIdentifier == 'nl.superalloy.oss.terminal-notifier'" 2>/dev/null | awk 'NR==1')
+			if [[ "$tn_loc" == "" ]] ; then
+				tn_status="osa"
+			else
+				tn_status="tn-app-old"
+			fi
+		else
+			tn_status="tn-app-new"
+		fi
 	fi
 fi
 
-# notify: exchange subtitle & message text for the actual strings
-notify "subtitle" "message text"
+# notify: exchange subtitle & message text for the actual strings (using test notification)
+notify "Test notification" "Status: $tn_status"
+
+# print variables (only for testing)
+echo "\$PATH: $tn"
+echo "Version: $tn_vers"
+echo ".app executable location: $tn_loc"
+echo "Status: $tn_status"
+
+# test notification directly from $PATH
+terminal-notifier -title "$process [$account]" -subtitle "Test notification" -message "$tn" -appIcon "$icon_loc"
